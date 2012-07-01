5bfc93021ead62189da8b94111856044ebf988ae
Keith Salisbury <keithsalisbury@gmail.com>
2012-06-30 23:38:43 -1000

    added new button and new class for osm data structures

---
+++ src/osm.h
@@ -0,0 +1,53 @@
+/******************************************************************************
+ * $Id: osm.h,v 1.0 2011/02/26 01:54:37 ktec Exp $
+ *
+ * Project:  OpenCPN
+ * Purpose:  OSM Plugin
+ * Author:   Keith Salisbury
+ *
+ ***************************************************************************
+ *   Copyright (C) 2012 by Keith Salisbury                                 *
+ *   $EMAIL$                                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+ ***************************************************************************
+ */
+
+#ifndef _OSM_H_
+#define _OSM_H_
+
+#include <wx/wxprec.h>
+
+#ifndef  WX_PRECOMP
+  #include <wx/wx.h>
+#endif //precompiled headers
+
+//WX_DECLARE_STRING_HASH_MAP( wxString, SeamarkTypesHashMap );
+//const SeamarkTypesHashMap* SeamarkTypes;
+
+
+struct Node
+{
+    long long id;
+    wxString seamark_type;
+	double latitude;
+	double longitude;
+};
+
+
+
+
+#endif
